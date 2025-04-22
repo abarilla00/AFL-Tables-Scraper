c31e574fad05e52932c4d4103fa4d8ad013d6c63
Antonio Barilla <baray079@mymail.unisa.edu.au>
2025-04-22 20:19:29 +0930

    <Add> Code comments and team registry implementation

--- afl_content/classes.py
+++ afl_content/classes.py
@@ -1,47 +1,68 @@
 from dataclasses import dataclass
 
 class Team:
-    def __init__(self):
-        pass
+    def __init__(self, name: str):
+        self.name = name
+        self.matches = {}
+
+    def add_match(self, round_name: str, id: int):
+        """
+        Adds a match's id to a team's list of played matches for easy access
+        """
+        self.matches[round_name] = id
+        
 
 class Match:
     def __init__(self, match_content: dict):
-        self.home_team = match_content["home_team"]
-        self.away_team = match_content["away_team"]
+        self.home_team_name = match_content["home_team"]
+        self.away_team_name = match_content["away_team"]
+              
         self.score = match_content["score"]
         self.venue = match_content["venue"]
         self.date_time = match_content["date_time"]
         self.id = match_content["id"]
         self.attendance = match_content["attendance"]
-        self.winner = ""
-        
-        if self.away_team != "Bye":
-            self.winner = self.calculate_winner()
+
+        self.is_bye = True if self.away_team_name == "Bye" else False
+        self.winner = self.calculate_winner() if not self.is_bye else ""   
 
     def calculate_winner(self):
+        """
+        Calculates the winner of a given match using the stored score variable
+        """
+
+        #Cleaning stored score data
         home_score = int(self.score["FT"].split(" - ")[0].split(".")[2])
         away_score = int(self.score["FT"].split(" - ")[1].split(".")[2])
         
         if home_score > away_score:
-            return self.home_team
+            return self.home_team_name
         elif home_score < away_score:
-            return self.away_team
+            return self.away_team_name
         else:
             return
     
     def __str__(self):
-        if self.away_team == "Bye":
-            return f"[Bye] {self.home_team}"
+        """
+        Overriding the default string representation of this object to return match content cleanly
+        """
+        if self.is_bye:
+            return f"[Bye] {self.home_team_name}"
         else:
-            return f"{self.home_team} ({self.score['FT'].split(' - ')[0]}) {'def.' if self.winner == self.home_team else 'def by.' if self.winner == self.away_team else 'tied.'} {self.away_team} ({self.score['FT'].split(' - ')[1]}) @ {self.venue}"
+            return f"{self.home_team_name} ({self.score['FT'].split(' - ')[0]}) {'def.' if self.winner == self.home_team_name else 'def by.' if self.winner == self.away_team_name else 'tied.'} {self.away_team_name} ({self.score['FT'].split(' - ')[1]}) @ {self.venue}"
 
 
 class TeamInMatch:
+
+    def __init__(self, team: Team, id: int):
+        self.team = team
+        self.id = id
+        self.stats = ""
+
+    def build(self):
+        pass
     @dataclass(frozen=True)
     class TeamStats:
-        pass
-
-    def __init__(self):
         pass
 
 class Round:
@@ -55,11 +76,17 @@ class Round:
         print(self)
     
     def build(self, round_content: list):
+        """
+        Builds the round's match list
+        """
         
         for match in round_content:
             self.matches.append(Match(match))
 
     def __str__(self):
+        """
+        Overriding the default string representation of this object to return a round's content cleanly
+        """
         output = f"{self.name}\n\n"
 
         for match in self.matches:
--- afl_content/controller.py
+++ afl_content/controller.py
@@ -7,14 +7,38 @@ class Controller:
         self.team_registry = {}
         self._match_cache = {}
     
-    def build(self):
-        regular_season = utilities.Scraper.scrape_season(2024)
+    def build(self, year: int):
+        regular_season = utilities.Scraper.scrape_season(year)
         
         for round in regular_season:
             round_struct = utilities.RoundParser.parse(round)
+            
+            if not self.team_registry:
+                
+                for match in round_struct[1:]:
+                    self.team_registry[match["home_team"]] = Team(match["home_team"])
+                    if match["away_team"] != "Bye":
+                        self.team_registry[match["away_team"]] = Team(match["away_team"])            
+
             self.rounds.append(Round(round_struct))
         
+        self.fill_team_registry()
+        
+
+    def fill_team_registry(self):
+        
+        for round in self.rounds:
+            for match in round.matches:
+                
+                if match.is_bye:
+                    self.team_registry[match.home_team_name].add_match(round.name, 0)
+                
+                else:
+                    self.team_registry[match.home_team_name].add_match(round.name, match.id)
+                    self.team_registry[match.away_team_name].add_match(round.name, match.id)
+
+                  
 
 if __name__ == "__main__":
     controller = Controller()
-    controller.build()
+    controller.build(2024)
